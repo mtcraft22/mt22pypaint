8fe188546e6a7de5ab40a7ae351d74ec8cb25ec6
Marc Tort Pascual <marctorpas@campus.monlau.com>
2023-01-27 17:05:39 +0100

    hey he areglado el ctr-z

--- paint.py
+++ paint.py
@@ -2,10 +2,10 @@ from tkinter import *
 import queue
 from tkinter import colorchooser, messagebox
 
+eliminados = queue.LifoQueue()
 root = Tk()
 material = "lapis"
 col = "black"
-
 hancho = Scale(from_=0, to=100, orient=HORIZONTAL)
 hancho.grid(row=2, column=10, columnspan=8)
 dis_linias = Scale(root, from_=1, to=30, orient=HORIZONTAL)
@@ -30,36 +30,24 @@ boton_F0F0F0 = Button(root, bg="#F0F0F0", command=lambda: set_color("#F0F0F0"), 
 boton_F0F0F0.grid(row=3, column=12)
 boton_color = Button(root, bg="#F0F0F0", command=lambda: asck_color(), height=1, width=1)
 boton_color.grid(row=4, column=12)
-
-
-
-    
 def asck_color():
     c2 = colorchooser.askcolor()[1]
     print(c2)
     set_color(c2)
     boton_color.config(bg=c2)
-
-
 def set_color(c):
     print(c)
     global col
     col = c
     boton_color.config(bg=c)
-
-
 cord = []
-
-
 def des():
     avd = messagebox.askquestion("Adventecia", "Si elimina eliminara el buffer ctr-y/z continuar?")
     if avd == "yes":
-        eliminados = queue.Queue()
+        eliminados = queue.LifoQueue()
         lienzo.delete("all")
     else:
         return
-
-
 def set_material(m):
     global material
     material = m
@@ -96,77 +84,51 @@ def set_material(m):
 def clear(event):
     cord.clear()
 
-
 def motion2(event):
-    
     x, y = event.x, event.y
-    lienzo.create_rectangle(x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(), fill=col,
-                            outline=col, tags=(col, "lapis",0, x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get()))
+    lienzo.create_rectangle(x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(), fill=col, outline=col, tags=(col, "lapis",0, x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get()))
     cord.clear()
 
 def motion(event):
-    
     x, y = event.x, event.y
-    lienzo.create_rectangle(x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(), fill="white",
-                            outline="white", tags=(col, "goma",0,x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(),))
+    lienzo.create_rectangle(x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(), fill="white", outline="white", tags=("white", "goma",0,x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get()))
     cord.clear()
-
-
-
 
 def linea(event):
     x, y = event.x, event.y
     cord.append(x)
     cord.append(y)
     if len(cord) == 4 or len(cord) > 4:
-        lienzo.create_line(cord[0], cord[1], cord[2], cord[3], fill=col, tags=(col, "linea", hancho.get() * 2,cord[0], cord[1], cord[2], cord[3]),
-                            width=hancho.get() * 2)
+        lienzo.create_line(cord[0], cord[1], cord[2], cord[3], fill=col, tags=(col, "linea", hancho.get() * 2,cord[0], cord[1], cord[2], cord[3]), width=hancho.get() * 2)
         cord.clear()
-
-
-
-
 
 def recta(event):
     x, y = event.x, event.y
     cord.append(x)
     cord.append(y)
     if len(cord) == 4 or len(cord) > 4:
-        lienzo.create_rectangle(cord[0], cord[1], cord[2], cord[3], outline=col, width=hancho.get() * 2,
-                                tags=(col, "rect", hancho.get() * 2, cord[0], cord[1], cord[2], cord[3] ))
+        lienzo.create_rectangle(cord[0], cord[1], cord[2], cord[3], outline=col, width=hancho.get() * 2,tags=(col, "rect", hancho.get() * 2, cord[0], cord[1], cord[2], cord[3] ))
         cord.clear()
-
 
 def spray(event):
     x, y = event.x, event.y
     dis = (hancho.get() * 2) + dis_linias.get()
-    lienzo.create_rectangle(x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(), fill=col,
-                            outline=col, tags=col)
-    lienzo.create_rectangle(x - hancho.get() + dis, y + hancho.get() - dis, x + hancho.get() + dis,
-                            y - hancho.get() - dis, fill=col, outline=col, tags=col)
-
+    lienzo.create_rectangle(x - hancho.get(), y + hancho.get(), x + hancho.get(), y - hancho.get(), fill=col,outline=col, tags=col)
+    lienzo.create_rectangle(x - hancho.get() + dis, y + hancho.get() - dis, x + hancho.get() + dis, y - hancho.get() - dis, fill=col, outline=col, tags=col)
 
 def getcolor(event):
     for item_id in lienzo.find_all():
         tag = lienzo.gettags(item_id)[0]
         lienzo.tag_bind(tag, '<Button-2>', lambda _, i=tag: set_color(i))
 
-
-eliminados = queue.LifoQueue()
-
-
-
-
+open("datos.dat","a")
 def detraas(event):
     try:
-
-
         if lienzo.gettags(lienzo.find_all()[-1])[1]=="lapis":
             for i in range(10):   
                 eliminados.put(lienzo.gettags(lienzo.find_all()[-1]))
                 lienzo.delete(lienzo.find_all()[-1])
-                eliminados.append((lienzo.gettags(lienzo.find_all()[-1])[1], lienzo.coords(lienzo.find_all()[-1]),
-                                lienzo.gettags(lienzo.find_all()[-1])[0],lienzo.find_all()[-1]))
+              
         else:
             eliminados.put(lienzo.gettags(lienzo.find_all()[-1]))
             lienzo.delete(lienzo.find_all()[-1])
@@ -178,37 +140,28 @@ def detraas(event):
 tags=(col, "linea", hancho.get() * 2, cord)
 lienzo.create_rectangle(cord[0], cord[1], cord[2], cord[3], outline=col, width=hancho.get() * 2,tags=(col, "rect", hancho.get() * 2, cord ))'''
 def alante(event):
+    if eliminados.empty():
+            messagebox.showinfo("Info","No existen elementos a reustarar")
+            return
     item = eliminados.get()
-    materia= item[1]
-    if materia == "lapis":
+    materia = item[1]
+    if materia == "rect":
+        lienzo.create_rectangle(item[3],item[4],item[5],item[6],outline=item[0],width=item[2],tags=item)
+    elif materia == "linea":
+        lienzo.create_line(item[3],item[4],item[5],item[6],fill=item[0],width=item[2],tags=item)
+    elif materia == "lapis":
         for i in range(10):
             print(item)
             lienzo.create_rectangle(item[3],item[4],item[5],item[6],outline=item[0],fill=item[0],width=item[2],tags=item)
             item = eliminados.get()
             materia= item[1]  
             if eliminados.empty():
-                messagebox.showinfo("Info","No existen elemntos a reustarar")
+                messagebox.showinfo("Info","No existen elementos a reustarar")
                 return
-    else:
-        if materia == "rect":
-            if eliminados.empty():
-                messagebox.showinfo("Info","No existen elemntos a reustarar")
-                return
-            lienzo.create_rectangle(item[3],item[4],item[5],item[6],outline=item[0],width=item[2],tags=item)
-        elif materia == "linea":
-            if eliminados.empty():
-                messagebox.showinfo("Info","No existen elemntos a reustarar")
-                return
-            lienzo.create_line(item[3],item[4],item[5],item[6],fill=item[0],width=item[2],tags=item)
-        
-                
-        
-
-
+            
 lienzo.bind('<Button-2>', getcolor)
 lienzo.bind("<B3-Motion>", spray)
 root.bind("<Control-z>", detraas)
 root.bind("<Control-y>", alante)
-
 root.mainloop()
 
--- tabs.py
+++ tabs.py
@@ -2,8 +2,18 @@ import tkinter
 from tkinter import ttk
 
 root = tkinter.Tk()
-
 tab = ttk.Notebook(root)
+class Tabs:
+    def __init__(self,**kwargs):
+        self.name=kwargs["name"]
+        self.height=kwargs["height"]
+        self.width=kwargs["width"]
+        self.notebook=kwargs["notebook"]
+        self.frame=tkinter.Frame(self.tabadded, width=200, height=200)
+        self.tabadded=kwargs["tabadded"]
+
+
+
 tab.pack(pady=15)
 marco1 = tkinter.Frame(tab, width=200, height=200)
 marco2 = tkinter.Frame(tab, width=200, height=200)
@@ -12,7 +22,7 @@ marco2.pack(expand=1, fill="both")
 tab.add(marco1, text="saludar")
 tab.add(marco2, text="despedir")
 
-saludar = tkinter.Button(marco1, text="saludat", command=lambda: print("hey"))
+saludar = tkinter.Button(marco1, text="saludar", command=lambda: print("hey"))
 despedir = tkinter.Button(marco2, text="despedir", command=lambda: print("dios"))
 saludar.pack()
 despedir.pack()
